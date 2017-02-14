3b6b617fc4ca6fe42cb73b00cf8bfedc601ec689
Jorchie <jacortez_94@hotmail.com>
2017-02-13 18:54:38 -0600

    Tutorial 2 Updated
    Set the codes, tiles and project for the 2nd tutorial

---
+++ Tutorial2/UnityProject/Assets/crateMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class crateMove : MonoBehaviour {
+
+    //Where is it gonna move towards?
+    public Vector3 Myvector3;
+    //Rigid body
+    Rigidbody2D myrb2d;
+    
+
+	// Use this for initialization
+	void Start () {
+        //Place our rigid body 2d component into the myrb2d 
+        //variable so we can interact with it
+        myrb2d = GetComponent<Rigidbody2D>();
+        //Sets the velocity to be equals to the
+        //speed you place in the axis you select
+        myrb2d.velocity = Myvector3;
+
+	}
+}
---
+++ Tutorial2/UnityProject/Assets/rotationScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationScript : MonoBehaviour 
+{
+    //Declare our variables
+    public float x;
+    public float y;
+    public float z;
+	
+	// Update Method is called once per frame
+	void Update () {
+        //transforms the object's rotation to the axis you preffer 
+        transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+	}
+}
