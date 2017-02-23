b0cf45eed2046500acfa960e8b0190c50dcd2690
Jorchie <jacortez_94@hotmail.com>
2017-02-23 14:12:16 -0600

    Tutorial #3
    The Assets of the Unity Tutorial #3
    -Pill and Sphere prefabs
    -Shoot Script
    -BulletLifeTime Script
    -Cannon
    -Test Scene
    -Simple Material

---
+++ Unity Tutorial #3/Assets/Scripts/BulletLifetime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLifetime : MonoBehaviour {
+
+    //Destroy method
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Unity Tutorial #3/Assets/Scripts/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    //Bullet prefab
+    public GameObject Bullet;
+
+    //Spawn point
+    public GameObject SpawnPoint;
+
+    //Speed
+    public float ShootSpeed = 200;
+
+    //Bullet holder
+    GameObject Clone;
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //print("Bullet has been shoot");
+            //instantiate the projectile at the spownpoint position
+            Clone = Instantiate(Bullet, SpawnPoint.transform.position, SpawnPoint.transform.rotation);
+
+            //Adding the force to our bullet
+            Rigidbody CloneRb = Clone.GetComponent<Rigidbody>();
+            CloneRb.AddForce(Clone.transform.forward * ShootSpeed);
+            
+        }
+
+    }
+}
